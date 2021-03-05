0ad4282ed17afd7df2c7131fd0c0ed9db55056b5
lucklyYong <luckyzangvip@163.com>
2021-03-05 17:37:26 +0800

    测试代码提交

--- pages/callTask/callTask.vue
+++ pages/callTask/callTask.vue
@@ -268,6 +268,7 @@
 				<button class="cancelBtn" type="primary" @click="cancel">取消</button>
 			</view>
 		</view>
+		// 测试 git
 		<!-- 	<view class="bottom-bar">
 			<bottom-bar :itemIndex="0" @itemEvent="clickEvent"></bottom-bar>
 		</view> -->
