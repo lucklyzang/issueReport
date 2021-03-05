951f05c6bb10c47f552e1c6e600eee83e20d4314
lucklyYong <luckyzangvip@163.com>
2021-03-05 17:42:42 +0800

    取消测试git提交

--- pages/callTask/callTask.vue
+++ pages/callTask/callTask.vue
@@ -268,7 +268,6 @@
 				<button class="cancelBtn" type="primary" @click="cancel">取消</button>
 			</view>
 		</view>
-		// 测试 git
 		<!-- 	<view class="bottom-bar">
 			<bottom-bar :itemIndex="0" @itemEvent="clickEvent"></bottom-bar>
 		</view> -->
